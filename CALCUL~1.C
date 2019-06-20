8d3a8b9ce6d9fa4342081664efef758d707e7502
Nikhil Wadhwa <nikhilwadhwa887@gmail.com>
2019-06-20 11:06:46 +0530

    calculator

---
+++ CALCUL~1.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{  clrscr();
+   double a,b,result=0;
+   int choice;
+   printf("Enter two numbers: ");
+   scanf("%lf%lf", &a,&b);
+   printf("Enter your choice(1-4): ");
+   printf("\n1 for Addition.\n2 for Subtraction.\n3 for Multiplication.");
+   printf("\n4 for Division.\n");
+   scanf("%d",&choice);
+   switch(choice){
+case 1: result = a+b;
+	printf("\nThe sum of %lf and %lf is: %lf",a,b,result);
+	break;
+case 2: result = a-b;
+	printf("\nThe difference of %lf and %lf is: %lf",a,b,result);
+	break;
+case 3: result = a*b;
+	printf("\nThe product of %lf and %lf is: %lf",a,b,result);
+	break;
+case 4: result = a/b;
+	printf("\nThe quotient of %lf and %lf is: %lf",a,b,result);
+	break;
+case 5: result = a%b;
+       printf("\n The modulus of %lf and %lf is : %lf",a,b,result)
+       break;
+default:"invalid choice";
+}
+getch();
+}
